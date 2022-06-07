f0f2e31f540f52e191e5d5ca8236864a6442f126
Esther Verreau <everreau@ucop.edu>
2022-06-07 16:55:48 -0500

    add some basic interface

--- eschol/views.py
+++ eschol/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from plugins.eschol import forms
 
 from submission.models import Article
+from journal.models import Issue
 from core.models import File
 from core import files
 from django.http import Http404
@@ -13,15 +14,47 @@ from datetime import datetime, timedelta
 
 from .models import AccessToken
 
-def eschol_manager(request):
-    form = forms.EscholAdminForm()
+from .logic import issue_to_eschol, article_to_eschol
 
-    template = 'eschol/manager.html'
+def publish_issue(request, issue_id):
+    template = 'eschol/published.html'
+    issue = get_object_or_404(Issue, pk=issue_id)
+    msg = issue_to_eschol(issue=issue)
     context = {
-        'form': form,
+        'obj': issue,
+        'message': msg
+    }
+    return render(request, template, context)
+
+def publish_article(request, article_id):
+    template = 'eschol/published.html'
+    article = get_object_or_404(Article, pk=article_id)
+    msg = article_to_eschol(article=article)
+    context = {
+        'obj': article,
+        'message': msg
+    }
+    return render(request, template, context)
+
+def list_articles(request, issue_id):
+    template = 'eschol/list_articles.html'
+    issue = get_object_or_404(Issue, pk=issue_id)
+    context = {
+        'issue': issue,
+        'articles': issue.get_sorted_articles()
     }
 
     return render(request, template, context)
+
+def eschol_manager(request):
+    template = 'eschol/manager.html'
+    # get current journsl?  Look at OAI
+    context = {
+        'issues': Issue.objects.all()
+    }
+
+    return render(request, template, context)
+
 
 def access_article_file(request, article_id, file_id):
     if not "access" in request.GET:
