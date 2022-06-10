a040449c319f910694401ca45f9d2770724b2b1d
Esther Verreau <everreau@ucop.edu>
2022-06-10 11:49:29 -0500

    unit codes must be longer

--- eschol/models.py
+++ eschol/models.py
@@ -5,7 +5,7 @@ from secrets import token_urlsafe
 
 class JournalUnit(models.Model):
     journal = models.OneToOneField(Journal, null=True, on_delete=models.CASCADE)
-    unit = models.CharField(max_length=20)
+    unit = models.CharField(max_length=50)
 
     def __str__(self):
         return "{}: {}".format(self.journal, self.unit)
