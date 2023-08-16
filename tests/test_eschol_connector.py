f9b55dd42a31dc412efe936540cd6156219cf0d6
Esther Verreau <esther.verreau@ucop.edu>
2023-08-16 16:11:17 -0500

    more tests

--- tests/test_eschol_connector.py
+++ tests/test_eschol_connector.py
@@ -10,12 +10,18 @@ from django.utils import timezone
 
 import utils
 
-from submission.models import STAGE_PUBLISHED
+from submission.models import STAGE_PUBLISHED, Licence, Keyword, Funder, Field, FieldAnswer
 
 #from django.core.management import call_command
 #from django.contrib.messages import get_messages
 
 from eschol.logic import *
+
+# TODO:
+# - HTML
+# - Plural section
+# - invalid license
+# - Supp Files & galley
 
 class EscholConnectorTest(TestCase):
 
@@ -37,6 +43,18 @@ class EscholConnectorTest(TestCase):
         self.article.owner = self.user
         self.article.save()
 
+    def test_data_availability(self):
+        field1 = Field.objects.create(journal=self.journal, press=self.press, name="Data Availability", kind="text", order=1, required=False)
+        answer1 = FieldAnswer.objects.create(field=field1, article=self.article, answer="Public repository")
+
+        field2 = Field.objects.create(journal=self.journal, press=self.press, name="Data URL", kind="text", order=2, required=False)
+        answer2 = FieldAnswer.objects.create(field=field2, article=self.article, answer="http://data.repo")
+
+        j, e = get_article_json(self.article, get_unit(self.journal))
+
+        self.assertEqual(j["dataAvailability"], "publicRepo")
+        self.assertEqual(j["dataURL"], "http://data.repo")
+
     def test_minimal_json(self):
         j, e = get_article_json(self.article, get_unit(self.journal))
         self.assertEqual(j["sourceName"], "janeway")
@@ -56,6 +74,85 @@ class EscholConnectorTest(TestCase):
         self.assertEqual(len(j["localIDs"]), 1)
         self.assertEqual(j["localIDs"][0]["id"], f'janeway_{self.article.pk}')
         self.assertEqual(len(j), 15)
+
+    def test_kitchen_sink(self):
+        issue = helpers.create_issue(self.journal, articles=[self.article])
+        license, _ = Licence.objects.get_or_create(journal=self.journal,
+                                                   press=self.press,
+                                                   name="Creative Commons 4",
+                                                   short_name="CC4",
+                                                   url="https://creativecommons.org/licenses/by/4.0")
+        license.save()
+        author = helpers.create_author(self.journal)
+        funder, _ = Funder.objects.get_or_create(name="Test Funder", fundref_id="http://dx.doi.org/10.13039/501100021082")
+        funder.save()
+
+        self.article.abstract = "This is the abstract"
+        self.article.date_submitted = datetime(2022, 1, 1, tzinfo=timezone.get_current_timezone())
+        self.article.date_accepted = datetime(2022, 2, 2, tzinfo=timezone.get_current_timezone())
+        self.article.custom_how_to_cite = "Citation, blah. 2023"
+        self.article.first_page = 1
+        self.article.last_page = 2
+        self.article.language = "eng"
+        self.article.publisher_name = "The Publisher Name"
+        self.article.keywords.add(Keyword.objects.create(word="keyword1"))
+        self.article.license = license
+        self.article.authors.add(author)
+        self.article.funders.add(funder)
+
+        self.article.save()
+        author.snapshot_self(self.article)
+
+        self.journal.issn = "1111-1111"
+        self.journal.save()
+
+        j, e = get_article_json(self.article, get_unit(self.journal))
+        self.assertEqual(j["sourceName"], "janeway")
+        self.assertEqual(j["sourceID"], str(self.article.pk))
+        self.assertEqual(j["sourceURL"], self.press.domain)
+        self.assertEqual(j["submitterEmail"], self.user.email)
+        self.assertEqual(j["title"], self.article.title)
+        self.assertEqual(j["type"], "ARTICLE")
+        self.assertEqual(j["published"], self.article.date_published.strftime("%Y-%m-%d"))
+        self.assertEqual(j["datePublished"], self.article.date_published.strftime("%Y-%m-%d"))
+        self.assertEqual(j["isPeerReviewed"], self.article.peer_reviewed)
+        self.assertEqual(j["contentVersion"], "PUBLISHER_VERSION")
+        self.assertEqual(j["journal"], self.journal.name)
+        self.assertEqual(len(j["units"]), 1)
+        self.assertEqual(j["units"][0], self.journal.code)
+        self.assertEqual(j["pubRelation"], "EXTERNAL_PUB")
+        self.assertEqual(j["abstract"], "This is the abstract")
+        self.assertEqual(j["dateSubmitted"], "2022-01-01")
+        self.assertEqual(j["dateAccepted"], "2022-02-02")
+        self.assertEqual(j["customCitation"], "Citation, blah. 2023")
+        self.assertEqual(j["fpage"], '1')
+        self.assertEqual(j["lpage"], '2')
+        self.assertEqual(j["language"], 'eng')
+        self.assertEqual(j["sectionHeader"], 'Article')
+        self.assertEqual(len(j["keywords"]), 1)
+        self.assertEqual(j["keywords"][0], 'keyword1')
+        self.assertEqual(j["rights"], 'https://creativecommons.org/licenses/by/4.0/')
+        self.assertEqual(j["publisher"], 'The Publisher Name')
+        self.assertEqual(j["volume"], '0')
+        self.assertEqual(j["issue"], '0')
+        self.assertEqual(j["issueTitle"], 'Test Issue from Utils Testing Helpers')
+        self.assertEqual(j["issueDate"], '2022-01-01')
+        self.assertEqual(j["orderInSection"], 10001)
+        self.assertEqual(len(j["localIDs"]), 1)
+        self.assertEqual(j["localIDs"][0]["id"], f'janeway_{self.article.pk}')
+        self.assertEqual(len(j["authors"]), 1)
+        self.assertEqual(j["authors"][0]['nameParts']['fname'], "Author")
+        self.assertEqual(j["authors"][0]['nameParts']['lname'], "User")
+        self.assertEqual(j["authors"][0]['nameParts']['institution'], "Author institution")
+        self.assertEqual(j["authors"][0]['nameParts']['mname'], "A")
+        self.assertEqual(j["authors"][0]['email'], "authoruser@martineve.com")
+        self.assertEqual(len(j["grants"]), 1)
+        self.assertEqual(j["grants"][0]["name"], "Test Funder")
+        self.assertEqual(j["grants"][0]["reference"], "http://dx.doi.org/10.13039/501100021082")
+
+        self.assertEqual(len(j), 32)
+
+
 
     @patch.object(utils.logger.PrefixedLoggerAdapter, 'error')
     def test_send_article_no_issue(self, error_mock):
