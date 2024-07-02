db401bae8410fddd5ccfe570761221e22db8838d
Esther Verreau <esther.verreau@ucop.edu>
2024-07-02 10:42:20 -0500

    make issue publishing async

--- views.py
+++ views.py
@@ -17,18 +17,31 @@ from .models import AccessToken
 from .logic import issue_to_eschol, article_to_eschol
 from .plugin_settings import PLUGIN_NAME
 
+from django_q.tasks import async_task
+from django_q.tasks import fetch_group
+
+def publish_issue_task(issue_id):
+    issue = Issue.objects.get(pk=issue_id)
+    epubs, errors = issue_to_eschol(issue=issue)
+    if len(errors) > 0:
+        return {"success": False, "result": f'Failed to publish {issue}: {";".join(errors)}'}
+    else:
+        return {"success": True, "result": f"Published {len(epubs)} articles in {issue}"}
+
+def publish_issue_result(task):
+    print("publish issue result")
+    result = task.result
+    task.success = result["success"]
+    task.result = result["result"]
+    task.hook = None
+    task.save()
+
 def publish_issue(request, issue_id):
-    template = 'eschol/published.html'
+    template = 'eschol/issue_publish_queued.html'
     issue = get_object_or_404(Issue, pk=issue_id)
-    epubs, errors = issue_to_eschol(request=request, issue=issue)
-    context = {
-        'plugin_name': PLUGIN_NAME,
-        'obj': issue,
-        'objs': epubs,
-        'errors': errors,
-        'issue': issue,
-        'obj_name': "Issue"
-    }
+    async_task('plugins.eschol.views.publish_issue_task', issue_id, group=issue_id, hook='plugins.eschol.views.publish_issue_result')
+    context = {'plugin_name': PLUGIN_NAME,
+               'issue': issue}
     return render(request, template, context)
 
 def publish_article(request, article_id):
@@ -51,7 +64,8 @@ def list_articles(request, issue_id):
     context = {
         'plugin_name': PLUGIN_NAME,
         'issue': issue,
-        'articles': issue.get_sorted_articles()
+        'articles': issue.get_sorted_articles(),
+        'tasks': fetch_group(issue_id),
     }
 
     return render(request, template, context)
